9d86aef259fcdf0db7d127a02cd3af2a37d9f6ec
Tim DE WINTER <tdewinter@roxwhale.com>
2018-08-29 19:48:37 +0200

    chore: Updated test call

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -5,7 +5,7 @@ import load from 'load-json-file';
 import path from 'path';
 import write from 'write-json-file';
 
-import simplepolygon from '..';
+import polygonComplex from '..';
 
 
 
@@ -41,7 +41,7 @@ function colorize(features, colors = ['#F00', '#00F', '#0F0', '#F0F', '#FFF'], w
 describe('polygon-complex', () =>  {
   test('should match', () => {
     for (const {filename, name, geojson} of fixtures) {
-      const results = colorize(simplepolygon(geojson));
+      const results = colorize(polygonComplex(geojson));
 
       if (process.env.REGEN) write.sync(directories.out + filename, results);
       expect(results).toEqual(load.sync(directories.out + filename), name);
