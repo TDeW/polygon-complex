e2f2b82d24d02163c2357ad7d6f840e16c1928a4
Tim DE WINTER <tdewinter@roxwhale.com>
2018-08-29 19:29:40 +0200

    chore: Moved tests to __tests__

---
+++ src/__tests__/index.js
@@ -0,0 +1,50 @@
+import { featureCollection } from '@turf/helpers';
+import { featureEach } from '@turf/meta';
+import fs from 'fs';
+import load from 'load-json-file';
+import path from 'path';
+import write from 'write-json-file';
+
+import simplepolygon from '..';
+
+
+
+const directories = {
+  in: path.join(__dirname, 'in') + path.sep,
+  out: path.join(__dirname, 'out') + path.sep
+};
+
+const fixtures = fs.readdirSync(directories.in).map(filename => {
+  return {
+    filename,
+    name: path.parse(filename).name,
+    geojson: load.sync(directories.in + filename)
+  };
+});
+
+function colorize(features, colors = ['#F00', '#00F', '#0F0', '#F0F', '#FFF'], width = 6) {
+  const results = [];
+  featureEach(features, (feature, index) => {
+    const color = colors[index % colors.length]
+    feature.properties = Object.assign(feature.properties, {
+      stroke: color,
+      fill: color,
+      'stroke-width': width,
+      'fill-opacity': 0.1
+    });
+    results.push(feature);
+  });
+  return featureCollection(results);
+}
+
+
+describe('polygon-complex', () =>  {
+  test('should match', () => {
+    for (const {filename, name, geojson} of fixtures) {
+      const results = colorize(simplepolygon(geojson));
+
+      if (process.env.REGEN) write.sync(directories.out + filename, results);
+      expect(results).toEqual(load.sync(directories.out + filename), name);
+    }
+  })
+});
