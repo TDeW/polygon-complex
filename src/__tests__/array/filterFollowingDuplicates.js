8516252238c2004cd5a488226eb61f79237ea40f
Tim DE WINTER <tdewinter@roxwhale.com>
2018-09-01 11:34:54 +0200

    feat: array/filterFollowingDuplicates

---
+++ src/__tests__/array/filterFollowingDuplicates.js
@@ -0,0 +1,239 @@
+import filterFollowingDuplicates from '../../array/filterFollowingDuplicates';
+
+
+
+describe('array/filterFollowingDuplicates', () => {
+
+  test('should be a function', () => {
+    expect(filterFollowingDuplicates).toBeInstanceOf(Function);
+  });
+
+  test('should do nothing on empty array', () => {
+    // PARAMS
+    const array = [];
+
+    // RESULT
+    const expected = [];
+
+
+    expect(filterFollowingDuplicates(array)).toEqual(expected);
+    expect(filterFollowingDuplicates(array)).not.toBe(expected); // immutable
+  });
+
+  test('should do nothing on one number array', () => {
+    // PARAMS
+    const array = [1];
+
+    // RESULT
+    const expected = [1];
+
+
+    expect(filterFollowingDuplicates(array)).toEqual(expected);
+    expect(filterFollowingDuplicates(array)).not.toBe(expected); // immutable
+  });
+
+  test('should do nothing on one object array', () => {
+    // PARAMS
+    const array = [{ val: 1 }];
+
+    // RESULT
+    const expected = [{ val: 1 }];
+
+
+    expect(filterFollowingDuplicates(array)).toEqual(expected);
+    expect(filterFollowingDuplicates(array)).not.toBe(expected); // immutable
+  });
+
+  test('should do nothing on one array array', () => {
+    // PARAMS
+    const array = [[1]];
+
+    // RESULT
+    const expected = [[1]];
+
+
+    expect(filterFollowingDuplicates(array)).toEqual(expected);
+    expect(filterFollowingDuplicates(array)).not.toBe(expected); // immutable
+  });
+
+  test('should filter one number on an pair of duplicate numbers', () => {
+    // PARAMS
+    const array = [1, 1];
+
+    // RESULT
+    const expected = [1];
+
+
+    expect(filterFollowingDuplicates(array)).toEqual(expected);
+    expect(filterFollowingDuplicates(array)).not.toBe(expected); // immutable
+  });
+
+  test('should filter one object on an pair of equal objects', () => {
+    // PARAMS
+    const array = [{ val: 1 }, { val: 1 }];
+
+    // RESULT
+    const expected = [{ val: 1 }];
+
+
+    expect(filterFollowingDuplicates(array)).toEqual(expected);
+    expect(filterFollowingDuplicates(array)).not.toBe(expected); // immutable
+  });
+
+  test('should filter one array on an pair of equal arrays', () => {
+    // PARAMS
+    const array = [[1], [1]];
+
+    // RESULT
+    const expected = [[1]];
+
+
+    expect(filterFollowingDuplicates(array)).toEqual(expected);
+    expect(filterFollowingDuplicates(array)).not.toBe(expected); // immutable
+  });
+
+  test('should do nothing an pair of non-duplicate numbers', () => {
+    // PARAMS
+    const array = [1, 2];
+
+    // RESULT
+    const expected = [1, 2];
+
+
+    expect(filterFollowingDuplicates(array)).toEqual(expected);
+    expect(filterFollowingDuplicates(array)).not.toBe(expected); // immutable
+  });
+
+  test('should do nothing an pair of not equal objects', () => {
+    // PARAMS
+    const array = [{ val: 1 }, { val: 2 }];
+
+    // RESULT
+    const expected = [{ val: 1 }, { val: 2 }];
+
+
+    expect(filterFollowingDuplicates(array)).toEqual(expected);
+    expect(filterFollowingDuplicates(array)).not.toBe(expected); // immutable
+  });
+
+  test('should do nothing an pair of not equal arrays', () => {
+    // PARAMS
+    const array = [[1], [2]];
+
+    // RESULT
+    const expected = [[1], [2]];
+
+
+    expect(filterFollowingDuplicates(array)).toEqual(expected);
+    expect(filterFollowingDuplicates(array)).not.toBe(expected); // immutable
+  });
+
+  test('should filter one number from an array of numbers with leading duplicates', () => {
+    // PARAMS
+    const array = [1, 1, 2];
+
+    // RESULT
+    const expected = [1, 2];
+
+
+    expect(filterFollowingDuplicates(array)).toEqual(expected);
+    expect(filterFollowingDuplicates(array)).not.toBe(expected); // immutable
+  });
+
+  test('should filter one object from an array of objects with leading duplicates', () => {
+    // PARAMS
+    const array = [{ val: 1 }, { val: 1 }, { val: 2 }];
+
+    // RESULT
+    const expected = [{ val: 1 }, { val: 2 }];
+
+
+    expect(filterFollowingDuplicates(array)).toEqual(expected);
+    expect(filterFollowingDuplicates(array)).not.toBe(expected); // immutable
+  });
+
+  test('should filter one array from an array of arrays with leading duplicates', () => {
+    // PARAMS
+    const array = [[1], [1], [2]];
+
+    // RESULT
+    const expected = [[1], [2]];
+
+
+    expect(filterFollowingDuplicates(array)).toEqual(expected);
+    expect(filterFollowingDuplicates(array)).not.toBe(expected); // immutable
+  });
+
+  test('should filter one number from an array of numbers with trailing duplicates', () => {
+    // PARAMS
+    const array = [1, 2, 2];
+
+    // RESULT
+    const expected = [1, 2];
+
+
+    expect(filterFollowingDuplicates(array)).toEqual(expected);
+    expect(filterFollowingDuplicates(array)).not.toBe(expected); // immutable
+  });
+
+  test('should filter one object from an array of objects with trailing duplicates', () => {
+    // PARAMS
+    const array = [{ val: 1 }, { val: 2 }, { val: 2 }];
+
+    // RESULT
+    const expected = [{ val: 1 }, { val: 2 }];
+
+
+    expect(filterFollowingDuplicates(array)).toEqual(expected);
+    expect(filterFollowingDuplicates(array)).not.toBe(expected); // immutable
+  });
+
+  test('should filter one array from an array of arrays with trailing duplicates', () => {
+    // PARAMS
+    const array = [[1], [2], [2]];
+
+    // RESULT
+    const expected = [[1], [2]];
+
+
+    expect(filterFollowingDuplicates(array)).toEqual(expected);
+    expect(filterFollowingDuplicates(array)).not.toBe(expected); // immutable
+  });
+
+  test('should filter one number from an array of numbers with median duplicates', () => {
+    // PARAMS
+    const array = [1, 2, 2, 3];
+
+    // RESULT
+    const expected = [1, 2, 3];
+
+
+    expect(filterFollowingDuplicates(array)).toEqual(expected);
+    expect(filterFollowingDuplicates(array)).not.toBe(expected); // immutable
+  });
+
+  test('should filter one object from an array of objects with median duplicates', () => {
+    // PARAMS
+    const array = [{ val: 1 }, { val: 2 }, { val: 2 }, { val: 3 }];
+
+    // RESULT
+    const expected = [{ val: 1 }, { val: 2 }, { val: 3 }];
+
+
+    expect(filterFollowingDuplicates(array)).toEqual(expected);
+    expect(filterFollowingDuplicates(array)).not.toBe(expected); // immutable
+  });
+
+  test('should filter one array from an array of arrays with median duplicates', () => {
+    // PARAMS
+    const array = [[1], [2], [2], [3]];
+
+    // RESULT
+    const expected = [[1], [2], [3]];
+
+
+    expect(filterFollowingDuplicates(array)).toEqual(expected);
+    expect(filterFollowingDuplicates(array)).not.toBe(expected); // immutable
+  });
+
+});
---
+++ src/array/filterFollowingDuplicates.js
@@ -0,0 +1,20 @@
+import complement from 'ramda/src/complement';
+
+import appendWhen from './appendWhen';
+import equalsLast from './equalsLast';
+import reduce from './reduce';
+
+
+
+/**
+* Remove the following duplicates of an Array
+* using deep equals comparison
+*
+* @param {Array}
+*
+* @return {Array}
+*/
+export default reduce(
+  appendWhen(complement(equalsLast)),
+  []
+);
