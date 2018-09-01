e3be92eebc2d6e9cb2de91a47336a3440777210b
Tim DE WINTER <tdewinter@roxwhale.com>
2018-09-01 11:57:27 +0200

    chore: made use of polygon/filterDuplicates

--- src/normalizeVertices.js
+++ src/normalizeVertices.js
@@ -1,43 +1,16 @@
-import append from 'ramda/src/append';
-import compose from 'ramda/src/compose';
 import concat from 'ramda/src/concat';
-import converge from 'ramda/src/converge';
-import dropLast from 'ramda/src/dropLast';
-import equals from 'ramda/src/equals';
-import head from 'ramda/src/head';
 import identity from 'ramda/src/identity';
-import last from 'ramda/src/last';
 import reduce from 'ramda/src/reduce';
 import useWith from 'ramda/src/useWith';
-import when from 'ramda/src/when';
 
-
-
-const filterExtremesDuplicates = when(
-  converge(equals, [head, last]),
-  dropLast(1)
-);
-
-const filterFollowingDuplicates = reduce(
-  (arr, val) => {
-    if (equals(last(arr), val)) return arr;
-    return append(val, arr);
-  },
-  []
-);
+import filterDuplicates from './polygon/filterDuplicates';
 
 
 
 export default reduce(
   useWith(
     concat,
-    [
-      identity,
-      compose(
-        filterFollowingDuplicates,
-        filterExtremesDuplicates
-      ),
-    ]
+    [identity, filterDuplicates]
   ),
   []
 );
